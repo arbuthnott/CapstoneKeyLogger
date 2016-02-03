fd75539e5e15a5ea69626f231176e8e45ae6c181
Chris Arbuthnott <chris.arbuthnott@gmail.com>
2016-02-03 10:28:16 -0400

    Starting to add some functionality to create, refresh, and delete the database from code, based on James ERD.

--- KeyManager/KeyManagerTestConsole/Program.cs
+++ KeyManager/KeyManagerTestConsole/Program.cs
@@ -13,7 +13,7 @@ namespace KeyManagerTestConsole
         {
             // Only needed the following line once.
             // the database is created in the Console Project /bin/Debug/
-            //SQLiteConnection.CreateFile("testdb.sqlite");
+            SQLiteConnection.CreateFile("testdb.sqlite");
 
             SQLiteConnection connection = new SQLiteConnection("Data Source=testdb.sqlite;Version=3;");
             connection.Open();
@@ -22,23 +22,23 @@ namespace KeyManagerTestConsole
             String sql = "CREATE TABLE student (first_name VARCHAR(20), last_name VARCHAR(20))";
             SQLiteCommand command = new SQLiteCommand(sql, connection);
             // only needed the following once.
-            //command.ExecuteNonQuery(); // apparently there are types of executes depending on query type/returns/etc
+            command.ExecuteNonQuery(); // apparently there are types of executes depending on query type/returns/etc
             // all the following INSERTs have the execution commented out to avoid duplicating.
             sql = "INSERT INTO student (first_name, last_name) values ('Michael', 'Ahearn')";
             command = new SQLiteCommand(sql, connection);
-            //command.ExecuteNonQuery();
+            command.ExecuteNonQuery();
             sql = "INSERT INTO student (first_name, last_name) values ('Chris', 'Arbuthnott')";
             command = new SQLiteCommand(sql, connection);
-            //command.ExecuteNonQuery();
+            command.ExecuteNonQuery();
             sql = "INSERT INTO student (first_name, last_name) values ('Jared', 'Everett')";
             command = new SQLiteCommand(sql, connection);
-            //command.ExecuteNonQuery();
+            command.ExecuteNonQuery();
             sql = "INSERT INTO student (first_name, last_name) values ('Ryan', 'Hackett')";
             command = new SQLiteCommand(sql, connection);
-            //command.ExecuteNonQuery();
+            command.ExecuteNonQuery();
             sql = "INSERT INTO student (first_name, last_name) values ('James', 'Hale')";
             command = new SQLiteCommand(sql, connection);
-            //command.ExecuteNonQuery();
+            command.ExecuteNonQuery();
 
             sql = "SELECT * FROM student WHERE first_name LIKE 'J%'";
             command = new SQLiteCommand(sql, connection);
@@ -48,6 +48,9 @@ namespace KeyManagerTestConsole
                 Console.WriteLine("First Name: " + reader["first_name"] + ", Last Name: " + reader["last_name"]);
             }
             connection.Close();
+
+            // test deletion.
+            //System.IO.File.Delete("testdb.sqlite");
 
             Console.Read();
         }
