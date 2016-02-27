74f73d4f14b4d517dc4d86e2fe805babd44b1ad1
jaranvil <jaranvil@gmail.com>
2016-02-27 12:21:43 -0400

    added a MDI parent form to test a new UI

---
+++ KeyManager/KeyManagerForm/MDI_ParentForm.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace KeyManagerForm
+{
+    partial class MDI_ParentForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // MDI_ParentForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(767, 559);
+            this.IsMdiContainer = true;
+            this.Name = "MDI_ParentForm";
+            this.Text = "MDI_ParentForm";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ KeyManager/KeyManagerForm/MDI_ParentForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KeyManagerForm
+{
+    public partial class MDI_ParentForm : Form
+    {
+        public MDI_ParentForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
