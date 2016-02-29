11870b4fe1ddb989546d64585186d6b94bef83af
jaranvil <jaranvil@gmail.com>
2016-02-29 17:08:15 -0400

    Search setup on the key log

--- KeyManager/KeyManagerForm/NewLookupForm.Designer.cs
+++ KeyManager/KeyManagerForm/NewLookupForm.Designer.cs
@@ -29,25 +29,29 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(NewLookupForm));
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.tbSearch = new System.Windows.Forms.TextBox();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.listKeys = new System.Windows.Forms.ListView();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
-            // textBox1
+            // tbSearch
             // 
-            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(156, 31);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(471, 29);
-            this.textBox1.TabIndex = 0;
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.tbSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbSearch.Location = new System.Drawing.Point(52, 62);
+            this.tbSearch.Name = "tbSearch";
+            this.tbSearch.Size = new System.Drawing.Size(660, 29);
+            this.tbSearch.TabIndex = 0;
+            this.tbSearch.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.tbSearch.Enter += new System.EventHandler(this.tbSearch_Enter);
+            this.tbSearch.Leave += new System.EventHandler(this.tbSearch_Leave);
             // 
             // pictureBox2
             // 
             this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
-            this.pictureBox2.Location = new System.Drawing.Point(114, 31);
+            this.pictureBox2.Location = new System.Drawing.Point(10, 62);
             this.pictureBox2.Name = "pictureBox2";
             this.pictureBox2.Size = new System.Drawing.Size(36, 30);
             this.pictureBox2.TabIndex = 5;
@@ -60,21 +64,44 @@
             this.listKeys.GridLines = true;
             this.listKeys.HotTracking = true;
             this.listKeys.HoverSelection = true;
-            this.listKeys.Location = new System.Drawing.Point(12, 67);
+            this.listKeys.Location = new System.Drawing.Point(12, 114);
             this.listKeys.Name = "listKeys";
             this.listKeys.ShowItemToolTips = true;
-            this.listKeys.Size = new System.Drawing.Size(775, 484);
+            this.listKeys.Size = new System.Drawing.Size(700, 558);
             this.listKeys.TabIndex = 7;
             this.listKeys.UseCompatibleStateImageBehavior = false;
+            this.listKeys.ColumnClick += new System.Windows.Forms.ColumnClickEventHandler(this.listKeys_ColumnClick);
+            this.listKeys.SelectedIndexChanged += new System.EventHandler(this.listKeys_SelectedIndexChanged);
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(12, 9);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(145, 26);
+            this.lblTitle.TabIndex = 8;
+            this.lblTitle.Text = "The Key Log";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(236, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Type a door, key serial, ring, person, or key type.";
             // 
             // NewLookupForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(799, 563);
+            this.ClientSize = new System.Drawing.Size(716, 684);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblTitle);
             this.Controls.Add(this.listKeys);
             this.Controls.Add(this.pictureBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.tbSearch);
             this.HelpButton = true;
             this.Name = "NewLookupForm";
             this.Text = "NewLookupForm";
@@ -87,8 +114,10 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox tbSearch;
         private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.ListView listKeys;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label label1;
     }
 }
--- KeyManager/KeyManagerForm/NewLookupForm.cs
+++ KeyManager/KeyManagerForm/NewLookupForm.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Collections;
 using KeyManagerData;
 using KeyManagerClassLib;
 
@@ -14,20 +15,20 @@ namespace KeyManagerForm
 {
     public partial class NewLookupForm : Form
     {
-        private ObjectHolder objects;
+        private ObjectHolder objects;        
 
         public NewLookupForm(ObjectHolder objects)
         {
             InitializeComponent();
             this.objects = objects;
 
- 
-            PopulateListKeys();
+            SetupList();
+            PopulateListKeys("");
+            lblTitle.Focus();
+            
         }
 
-
-
-        public void PopulateListKeys()
+        public void SetupList()
         {
             listKeys.View = View.Details;
             listKeys.GridLines = true;
@@ -38,11 +39,20 @@ namespace KeyManagerForm
             listKeys.Columns.Add("Ring", 100);
             listKeys.Columns.Add("Person", 150);
             listKeys.Columns.Add("Doors", 200);
-            
+
 
             ImageList imgList = new ImageList();
             imgList.ImageSize = new Size(1, 40);
             listKeys.SmallImageList = imgList;
+        }
+
+        public void PopulateListKeys(String searchString)
+        {
+            listKeys.Items.Clear();
+
+            bool doSearch = false;
+            if (searchString.Length > 0)
+                doSearch = true;
 
             Color lightBlue = Color.FromArgb(166, 227, 247);
             Color evenLighterBlue = Color.FromArgb(220, 241, 247);
@@ -65,17 +75,59 @@ namespace KeyManagerForm
                 if (key.KeyRing != null)
                     ring = key.KeyRing.Name;
 
-                String[] args = {key.Serial, key.KeyType.Name, ring, " ", doors};
-                ListViewItem row = new ListViewItem(args);
+                String[] args = { key.Serial, key.KeyType.Name, ring, " ", doors };
 
-                if (count % 2 == 0)
-                    row.BackColor = lightBlue;
+                if (doSearch)
+                {
+                    if (DoSearch(searchString, args))
+                    {
+                        // add key to list
+                        ListViewItem row = new ListViewItem(args);
+                        if (count % 2 == 0)
+                            row.BackColor = lightBlue;
+                        else
+                            row.BackColor = evenLighterBlue;
+                        count++;
+                        listKeys.Items.Add(row);
+                    }
+                }
                 else
-                    row.BackColor = evenLighterBlue;
-                count++;
-
-                listKeys.Items.Add(row);
+                {
+                    // add key to list
+                    ListViewItem row = new ListViewItem(args);
+                    if (count % 2 == 0)
+                        row.BackColor = lightBlue;
+                    else
+                        row.BackColor = evenLighterBlue;
+                    count++;
+                    listKeys.Items.Add(row);
+                }
             }
+        }      
+
+        public bool DoSearch(String searchString, String[] args)
+        {
+            String[] terms = searchString.Split(' ');
+            String simpifiedArgs = "";
+            for (int i = 0;i<args.Length;i++)
+            {
+                if (i==0)
+                    simpifiedArgs = args[i];
+                else
+                    simpifiedArgs = simpifiedArgs + " " + args[i];
+            }
+            String[] keyValues = simpifiedArgs.Split(' ');
+
+            foreach (String term in terms)
+            {
+                foreach (String val in keyValues)
+                {
+                    if (val.ToLower().Contains(term.ToLower()))
+                        return true;
+                }
+            }
+
+            return false;
         }
 
         private void NewLookupForm_Load(object sender, EventArgs e)
@@ -85,7 +137,27 @@ namespace KeyManagerForm
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
+            PopulateListKeys(tbSearch.Text);
+        }
 
+        private void listKeys_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listKeys_ColumnClick(object sender, ColumnClickEventArgs e)
+        {
+           
+        }
+
+        private void tbSearch_Enter(object sender, EventArgs e)
+        {
+                       
+        }
+
+        private void tbSearch_Leave(object sender, EventArgs e)
+        {
+                     
         }
     }
 }
