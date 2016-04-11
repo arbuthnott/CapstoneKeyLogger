9d7ad747144c7b33f0d3d19fbd5a8e0fba2b09f0
angelusbarney <angelusbarney@gmail.com>
2016-04-11 20:55:21 -0300

    Added code for handling Encryption using AES (Needs testing)

---
+++ KeyManager/KeyManagerHelper/Encrypt.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace KeyManagerHelper
+{
+    class Encrypt
+    {
+        MemoryStream stream;
+        Aes aes;
+        ICryptoTransform encrypt;
+        ICryptoTransform decrypt;
+        CryptoStream csEncrypt;
+        CryptoStream csDecrypt;
+        StreamWriter cwEncrypt;
+        StreamReader crDecrypt;
+
+        public Encrypt()
+        {
+            aes = Aes.Create();
+
+        }
+
+        public void SetKey(Byte[] key)
+        {
+            aes.Key = key;
+            encrypt = aes.CreateEncryptor();
+            decrypt = aes.CreateDecryptor();
+        }
+
+        public String EncryptString(String plaintext)
+        {
+            stream = new MemoryStream();
+            csEncrypt = new CryptoStream(stream, encrypt, CryptoStreamMode.Write);
+            cwEncrypt = new StreamWriter(csEncrypt);
+            cwEncrypt.Write(plaintext);
+
+            return Convert.ToBase64String(stream.ToArray());
+        }
+
+        public Byte[] EncryptBinaryString(String plaintext)
+        {
+            stream = new MemoryStream();
+            csEncrypt = new CryptoStream(stream, encrypt, CryptoStreamMode.Write);
+            cwEncrypt = new StreamWriter(csEncrypt);
+            cwEncrypt.Write(plaintext);
+
+            return stream.ToArray();
+        }
+
+        public String DecryptString(String ciphertext)
+        {
+            stream = new MemoryStream(Convert.FromBase64String(ciphertext));
+            csDecrypt = new CryptoStream(stream, decrypt, CryptoStreamMode.Read);
+            crDecrypt = new StreamReader(csDecrypt);
+
+            return crDecrypt.ReadToEnd();
+        }
+
+        public String DecryptBinaryString(Byte[] ciphertext)
+        {
+            stream = new MemoryStream(ciphertext);
+            csDecrypt = new CryptoStream(stream, decrypt, CryptoStreamMode.Read);
+            crDecrypt = new StreamReader(csDecrypt);
+
+            return crDecrypt.ReadToEnd();
+        }
+    }
+}
